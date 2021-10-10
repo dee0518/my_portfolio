00e9341e8f2b3ceadec09163f8505a255ec36cfa
dee0518 <dehya0518@gmail.com>
2021-10-10 23:18:58 +0900

    Add chatbot project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+
+// navigation
+import { MAIN, CHATBOT } from './navigation/CONSTANTS'
+
+// component
+import { Chatbot } from './Path'
+
+// css
+import './assets/fonts/fonts.css'
+import './commonStyle/index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact to={MAIN} component={Chatbot}/>
+        <Redirect path="*" to={MAIN}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
